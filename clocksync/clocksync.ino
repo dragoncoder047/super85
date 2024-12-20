609ed9fbeb0b0867d01727198882f0a4fc50ec13
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-12-20 14:43:50 -0500

    modify to get it to pwm right

--- clocksync/clocksync.ino
+++ clocksync/clocksync.ino
@@ -4,39 +4,58 @@
 // Line frequency is 60Hz
 #define LINE_TICKS 1
 
+#define DEBUG_LED 4
+#define PWM_PIN 1
+#define LINE_PULSE_PIN 2
+#define WHEEL_PULSE_PIN 0
+
 void setup() {
-    pinMode(1, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(0, INPUT);
-    pinMode(2, INPUT);
-    //setTimer0Prescale(1);
-    byte temp = TCCR0A;
-    temp &= ~(7<<CS00);
-    temp |= 1<<CS00;
-    TCCR0A = temp;
+    pinMode(PWM_PIN, OUTPUT);
+    pinMode(DEBUG_LED, OUTPUT);
+    pinMode(LINE_PULSE_PIN, INPUT);
+    pinMode(WHEEL_PULSE_PIN, INPUT);
+
+    TCCR1 = 1 << CS10 | 1 << PWM1A | 2 << COM1A0;  // turn off clock divisor, select PB1 as OC1A output
 }
 
 // Error < 0 means wheel is behind
 // Error > 0 means wheel is ahead
 int error = 0;
-byte pwm = 0;
 
-byte last_PINB = 0;
+#define SLOWDOWN_BY 128
+
+#define MIN_PWM 0
+#define MAX_PWM 10
+
+
+void do_pid() {
+    static int slowdown = 0;
+    int div_by = 0; // constrain(abs(error), 16, 128); // run the loop faster as error gets smaller
+    slowdown++;
+    if (slowdown < div_by) return;
+    slowdown = 0;
+
+    static byte pwm = MIN_PWM;
+
+    if (error > 0) {
+        if (pwm > MIN_PWM) pwm--;
+    } else {
+        if (pwm < MAX_PWM) pwm++;
+    }
+    OCR1A = pwm;
+}
 
 void loop() {
-    if (error > 0) {
-        if (pwm > 0) pwm--;
-    } else {
-        if (pwm < 255) pwm++;
-    }
-    analogWrite(1, pwm);
+    do_pid();
+
+    static byte last_PINB = 0;
     int cur_PINB = PINB;
-    if (cur_PINB & (1<<PB0) && !(last_PINB & (1<<PB0))) {
+    if (cur_PINB & (1 << WHEEL_PULSE_PIN) && !(last_PINB & (1 << WHEEL_PULSE_PIN))) {
         error += WHEEL_TICKS;
     }
-    if (cur_PINB & (1<<PB2) && !(last_PINB & (1<<PB2))) {
+    if (cur_PINB & (1 << LINE_PULSE_PIN) && !(last_PINB & (1 << LINE_PULSE_PIN))) {
         error -= LINE_TICKS;
     }
     last_PINB = cur_PINB;
-    digitalWrite(4, error < 0 ? HIGH : LOW);
+    digitalWrite(DEBUG_LED, error < 0 ? HIGH : LOW);
 }
